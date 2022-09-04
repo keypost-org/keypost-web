fdb84c775f5d3b0e4827560af98dbb5f3b9684fd
Jeff Rade <jeffrade@gmail.com>
2022-09-04 11:55:21 -0500

    Use webpack --env via docs for future KEYPOST_APP_HOST use

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,40 +1,46 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      }
+module.exports = (env: any) => {
+  // https://webpack.js.org/guides/environment-variables/ Use env.<YOUR VARIABLE> here:
+  console.log('KEYPOST_APP_HOST: ', env.KEYPOST_APP_HOST);
+  console.log('production: ', env.production || false);
+
+  return {
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+
+    plugins: [
+      // https://github.com/jantimon/html-webpack-plugin#options
+      new HtmlWebpackPlugin({
+        title: 'tutorial', 
+        template: 'src/custom.html',
+        filename: 'custom.html'}),
+      new HtmlWebpackPlugin({
+        title: 'Keypost Home',
+        template: 'src/index.html',
+        filename: 'index.html'})
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
 
-  plugins: [
-    // https://github.com/jantimon/html-webpack-plugin#options
-    new HtmlWebpackPlugin({
-      title: 'tutorial', 
-      template: 'src/custom.html',
-      filename: 'custom.html'}),
-    new HtmlWebpackPlugin({
-      title: 'Keypost Home',
-      template: 'src/index.html',
-      filename: 'index.html'})
-   ],
-
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    compress: true,
-    port: 4000,
-  },
+    devServer: {
+      static: path.join(__dirname, "dist"),
+      compress: true,
+      port: 4000,
+    },
+  };
 };
