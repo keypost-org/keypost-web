e70dcf8a1ffa77f2af78d2b914e2877f6f239ef4
Jeff Rade <jeffrade@gmail.com>
2022-09-25 19:47:21 -0500

    Replace with double quotes to resolve eslint errors

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,44 +1,44 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-const path = require('path');
+const path = require("path");
 
 module.exports = (env : any) => {
   // https://webpack.js.org/guides/environment-variables/ Use env.<YOUR VARIABLE> here:
-  console.log('KEYPOST_APP_HOST: ', env.KEYPOST_APP_HOST);
-  console.log('production: ', env.production || false);
+  console.log("KEYPOST_APP_HOST: ", env.KEYPOST_APP_HOST);
+  console.log("production: ", env.production || false);
 
   return {
-    entry: './src/index.ts',
+    entry: "./src/index.ts",
     module: {
       rules: [
         {
           test: /\.ts?$/,
-          use: 'ts-loader',
+          use: "ts-loader",
           exclude: /node_modules/,
         }
       ],
     },
     resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
+      extensions: [".tsx", ".ts", ".js"],
     },
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
       clean: true,
     },
 
     plugins: [
       // https://github.com/jantimon/html-webpack-plugin#options
       new HtmlWebpackPlugin({
-        title: 'custom',
-        template: 'src/custom.html',
-        filename: 'custom.html'
+        title: "custom",
+        template: "src/custom.html",
+        filename: "custom.html"
       }),
       new HtmlWebpackPlugin({
-        title: 'Keypost Home',
-        template: 'src/index.html',
-        filename: 'index.html'
+        title: "Keypost Home",
+        template: "src/index.html",
+        filename: "index.html"
       }),
       new CopyPlugin({
         patterns: [
